d95600d42788028bc753a5ccf6e7943beb3e89a2
dylansq <dylansquinn@gmail.com>
2023-06-05 00:03:55 -0700

    add dribble.tf build

---
+++ app/dribble.tf/build/precache-manifest.fc5a5dec96f92ff59a1ff14eee0dafa7.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1a252e635c625202ecdecbeecd4509",
+    "url": "./demo/0d881c0827014bf79d22.worker.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "./demo/0d881c0827014bf79d22.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ca0289a556cb67c5b770fbeb23c673",
+    "url": "./demo/index.html"
+  },
+  {
+    "revision": "df406578ae3bf6f084e0",
+    "url": "./demo/static/css/2.64119653.chunk.css"
+  },
+  {
+    "revision": "676865dff90cde5d1877",
+    "url": "./demo/static/css/main.163e5d67.chunk.css"
+  },
+  {
+    "revision": "df406578ae3bf6f084e0",
+    "url": "./demo/static/js/2.c8a78030.chunk.js"
+  },
+  {
+    "revision": "136af3508733df7f789276b7383f7212",
+    "url": "./demo/static/js/2.c8a78030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676865dff90cde5d1877",
+    "url": "./demo/static/js/main.19f2cfe1.chunk.js"
+  },
+  {
+    "revision": "8851ebf07e2390144d45",
+    "url": "./demo/static/js/runtime-main.4700507e.js"
+  },
+  {
+    "revision": "3ad45f8a6727a79d6c0864ce4efdc058",
+    "url": "./demo/static/media/airstrike.3ad45f8a.png"
+  },
+  {
+    "revision": "5fe7fe03692119a8eb3d901cb84d56d9",
+    "url": "./demo/static/media/back_scatter.5fe7fe03.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "./demo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "./demo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "./demo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "./demo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "./demo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9fb4c8acd1d809eeeb61901c888a02e1",
+    "url": "./demo/static/media/bread_bite.9fb4c8ac.png"
+  },
+  {
+    "revision": "1550592b065d23909176d46b9848a473",
+    "url": "./demo/static/media/class_icons_64.1550592b.png"
+  },
+  {
+    "revision": "db3416c606f3de2cb85e9e09a696142d",
+    "url": "./demo/static/media/classic.db3416c6.png"
+  },
+  {
+    "revision": "81103d19a619558f705f552b4aba56e0",
+    "url": "./demo/static/media/combine_ball.81103d19.png"
+  },
+  {
+    "revision": "d9a1ee9b259e3fefa1c35d7f5d572f6d",
+    "url": "./demo/static/media/crossbow_bolt.d9a1ee9b.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "./demo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "./demo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "./demo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "./demo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "./demo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "./demo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "./demo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "./demo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "./demo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "./demo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "./demo/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "88996152e0248a1ba0a3b6e12bce4f41",
+    "url": "./demo/static/media/physcannon.88996152.png"
+  },
+  {
+    "revision": "f8a96600851fa6cfd8eb22d9cbf4c59d",
+    "url": "./demo/static/media/slam.f8a96600.png"
+  },
+  {
+    "revision": "1eb536d95b8fe807a8b85be06888eaba",
+    "url": "./demo/static/media/snack_attack.1eb536d9.png"
+  },
+  {
+    "revision": "cb8e5767cec50c6796a933f374bbda6f",
+    "url": "./demo/static/media/stun.cb8e5767.png"
+  },
+  {
+    "revision": "cb8e3cec81b8fc62dddafee97d8ce8ce",
+    "url": "./demo/static/media/tide_turner.cb8e3cec.png"
+  }
+]);

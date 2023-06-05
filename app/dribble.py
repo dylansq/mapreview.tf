8891d13eca70db82f2e63fe3c8dcd45d5c79731a
dylansq <dylansquinn@gmail.com>
2023-06-05 00:04:09 -0700

    Update dribble.py

--- app/dribble.py
+++ app/dribble.py
@@ -14,11 +14,14 @@ def static_proxy(path):
     """serve static files"""
     file_name = path.split("/")[-1]
     primary_path = path.split("/")[0]
-
+    paths  = path.split("/")
+    print(paths)
     #Need to get tricky with static folder structure because flask was not properly handeling files outside of the default static folder
     #There is likely a better solution than this.
     if primary_path in ['js','css','media']:
-        dir_name = os.path.join(dribble.static_folder,'static', "/".join(path.split("/")[:-1]))
+        dir_name = os.path.join(dribble.static_folder,'../static', "/".join(path.split("/")[1:-1]))
     else:
-        dir_name = os.path.join(dribble.static_folder, "/".join(path.split("/")[:-1]))
+        dir_name = os.path.join(dribble.static_folder, "/".join(path.split("/")[1:-1]))
+    
+    
     return send_from_directory(dir_name, file_name)

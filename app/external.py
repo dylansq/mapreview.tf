62455f19dc1d3a0f8e9b2c0d2810138a8e87d7a9
dylansq <dylansquinn@gmail.com>
2023-08-23 11:06:25 -0700

    adjust algo

--- app/external.py
+++ app/external.py
@@ -475,11 +475,11 @@ def refresh_yt_stats():
         if days_old < 1:
             days_old = 1
 
-        likerate_score = min(1,2*float(_vid.yt_stats_likes)/(days_old))
+        likerate_score = min(1,float(_vid.yt_stats_likes)/(days_old))
         age_score = (1-min(1.0,days_old/2191))**4
         likes_score = min(1,(float(_vid.yt_stats_likes)/100)**0.5)
         views_score = min(1,(float(_vid.yt_stats_views)/2000))
-        ratio_score = max(1.0,10*float(_vid.yt_stats_likes)/float(_vid.yt_stats_views))
+        ratio_score = min(1.0,float(_vid.yt_stats_likes)/float(_vid.yt_stats_views))
 
         mrtf_rating_score_a1 = int(100*(age_score*2 + likes_score + views_score + ratio_score + chapter_score + likerate_score)/7)
         setattr(_vid,"mrtf_rating_score_a1",mrtf_rating_score_a1)
